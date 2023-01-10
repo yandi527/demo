fa519fc1771b3a863e66ed950bcbb35d2304822f
yandi527 <yandi.lin@thoughtworks.com>
2023-01-11 01:26:51 +0800

    Jenkinsfile29

--- pwd.sh
+++ pwd.sh
@@ -5,5 +5,5 @@ pwd
 ls
 ./gradlew clean compileJava compileTestJava bootJar --info
 echo "1111111111111111111111"
-cd /Users/yandi.lin/.jenkins/workspace/demo/build
+cd /Users/yandi.lin/.jenkins/workspace/demo/build/libs
 ls
