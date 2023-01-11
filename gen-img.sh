0cbda8362980ffcb98df6bbf9c75de8fe0b68d38
yandi527 <yandi.lin@thoughtworks.com>
2023-01-11 08:34:03 +0800

    Jenkinsfile34

--- gen-img.sh
+++ gen-img.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set +x
 ./gradlew clean compileJava compileTestJava bootJar --info
-docker build -t demo
+echo "+++++++++++++++++++++++"
+pwd
+docker build -t demo -f ./Dockerfile .
